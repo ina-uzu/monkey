6117d3960b590619191860d2c750b647ce2db57b
uzu.ina <uzu.ina@kakaocorp.com>
2020-08-03 14:11:51 +0900

    ch 3.5

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.daumkakao.com/uzu-ina/monkey/evaluator"
 	"github.daumkakao.com/uzu-ina/monkey/lexer"
 	"github.daumkakao.com/uzu-ina/monkey/parser"
 )
@@ -48,8 +49,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
